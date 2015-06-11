e3e0637b3986f8ed250290278c5a95451c610c81
rewfe <qrewfe@yandex.ru>
2015-06-11 04:30:50 +0300

    get location

--- LocationDemo/ViewController.h
+++ LocationDemo/ViewController.h
@@ -7,9 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
 
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
 
+@property (weak, nonatomic) IBOutlet UILabel *latitudeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *longitudeLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+
+- (IBAction)getCurrentLocation:(id)sender;
 @end
 
